6d887a404dd3bcfce63383970697f6c8ee895684
Beatriz Faria <166550950+S0L0B@users.noreply.github.com>
2026-03-01 08:39:48 -0300

    Add currency conversion calculations

---
+++ 05-variables.py
@@ -0,0 +1,7 @@
+p = int(input("quanto você tem em pesos?"))
+s = int(input("quanto você tem em soles?"))
+r = int(input("quanto você tem em reais?"))
+
+d = p * 0.00027 + s * 0.30 + r * 0.19 
+
+print(d)
